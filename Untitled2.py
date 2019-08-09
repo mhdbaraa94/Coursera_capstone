9b75d0d6afa0db767013f862ae60aae0369684b0
mohammadbaraa jadallah <43537477+mhdbaraa94@users.noreply.github.com>
2019-08-09 08:55:43 +0300

    coursera
    final projects

---
+++ Untitled2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Applied data science capstone
+
+# this notebook will used only for projects of the final course of IBM Data Science Professional Certificate
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course')
+
+
+# In[ ]:
+
+
+
+
